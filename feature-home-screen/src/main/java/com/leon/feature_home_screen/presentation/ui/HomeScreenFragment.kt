99674f717b2a9e323ee9a5d764cba1b72314b8f3
Leon4ikL <uraltsev_leon@icloud.com>
2022-03-04 18:21:49 +0400

    Начало создания UI HomeScreen

--- feature-home-screen/src/main/java/com/leon/feature_home_screen/presentation/ui/HomeScreenFragment.kt
+++ feature-home-screen/src/main/java/com/leon/feature_home_screen/presentation/ui/HomeScreenFragment.kt
@@ -1,10 +1,6 @@
 package com.leon.feature_home_screen.presentation.ui
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.leon.feature_home_screen.R
 
 
