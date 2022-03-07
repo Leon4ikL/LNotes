14bc9e680022d275e5f937b42229d54637ceb67f
Leon4ikL <uraltsev_leon@icloud.com>
2022-03-07 11:22:03 +0400

    Подключаю adapter delegate

--- feature-home-screen/src/main/java/com/leon/feature_home_screen/api/HomeScreenFragment.kt
+++ feature-home-screen/src/main/java/com/leon/feature_home_screen/api/HomeScreenFragment.kt
@@ -6,18 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.leon.feature_home_screen.R
+import com.leon.feature_home_screen.databinding.FragmentHomeScreenBinding
 
 
-class HomeScreenFragment : Fragment(R.layout.fragment_home_screen) {
+class HomeScreenFragment : Fragment() {
 
-    
+    private var _binding: FragmentHomeScreenBinding? = null
+    private val binding: FragmentHomeScreenBinding
+        get() = _binding ?: throw RuntimeException("FragmentHomeScreenBinding == null")
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        _binding = FragmentHomeScreenBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
