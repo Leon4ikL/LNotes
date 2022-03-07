1e8454e91e0c00ecc8e1e84da1b7d5da0931d0a5
Leon4ikL <uraltsev_leon@icloud.com>
2022-03-07 10:30:31 +0400

    Добавление view binding в home screen

--- feature-home-screen/src/main/java/com/leon/feature_home_screen/api/HomeScreenFragment.kt
+++ feature-home-screen/src/main/java/com/leon/feature_home_screen/api/HomeScreenFragment.kt
@@ -1,11 +1,27 @@
 package com.leon.feature_home_screen.api
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.leon.feature_home_screen.R
 
 
 class HomeScreenFragment : Fragment(R.layout.fragment_home_screen) {
 
+    
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 
 }
